0fa4ac40e1e8ce30b59e39641416a84fc1e26ea0
mark johnson <mjohnson24@comcast.net>
2024-08-25 18:04:53 -0500

    added destructuring methods

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ before.innerHTML = JSON.stringify(companiesData, null, 2);
 
 // const manipulatedData = Object.keys(extraRecord.fieldData);
 // const manipulatedData = Object.values(extraRecord.fieldData);
-extraRecord.fieldData.Country = "USA";
-extraRecord.fieldData.CityState = `${extraRecord.fieldData.City}, ${extraRecord.fieldData.State}`;
-delete extraRecord.fieldData.Id; // DELETES a key/value pair from the object
-const manipulatedData = extraRecord;
+// extraRecord.fieldData.Country = "USA";
+// extraRecord.fieldData.CityState = `${extraRecord.fieldData.City}, ${extraRecord.fieldData.State}`;
+// delete extraRecord.fieldData.Id; // DELETES a key/value pair from the object
+const { City, State, Zip } = extraRecord.fieldData; // DESTRUCTING
+console.log(City, State, Zip);
+const manipulatedData = extraRecord.fieldData;
 //TODO: add the manipulated data array to the 'after' div.
 const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
