47ad0bae2c5ca6be86a9fffe0aa06b214eede658
mark johnson <mjohnson24@comcast.net>
2024-08-25 17:31:08 -0500

    wrote object methods

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import { companiesData } from "./companies-data";
+import { extraRecord } from "./extra-record";
 
+console.log("Extra", extraRecord);
 const before = document.getElementById("before");
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 
-const manipulatedData = [];
+// const manipulatedData = Object.keys(extraRecord.fieldData);
+// const manipulatedData = Object.values(extraRecord.fieldData);
+extraRecord.fieldData.Country = "USA";
+extraRecord.fieldData.CityState = `${extraRecord.fieldData.City}, ${extraRecord.fieldData.State}`;
+delete extraRecord.fieldData.Id; // DELETES a key/value pair from the object
+const manipulatedData = extraRecord;
 //TODO: add the manipulated data array to the 'after' div.
 const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
