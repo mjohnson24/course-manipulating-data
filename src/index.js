70db0947ac624c013ec37772be9f0f512878730a
mark johnson <mjohnson24@comcast.net>
2024-08-31 19:37:57 -0500

    added push to add an extraRecord element to the original array

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import { companiesData } from "./companies-data";
+import { extraRecord } from "./extra-record";
 
 const before = document.getElementById("before");
 
 // const manipulatedData = companiesData[0];
 // const manipulatedData = companiesData.pop(); // removes the last element
-const manipulatedData = companiesData.shift(); // removes the first element
+// const manipulatedData = companiesData.shift(); // removes the first element
+const manipulatedData = companiesData.push(extraRecord); // adds an element to the end
 //TODO: add the manipulated data array to the 'after' div.
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 const after = document.getElementById("after");
