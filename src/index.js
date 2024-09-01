8ecc9af76ab0083df9cdc9535e8546b721a89564
mark johnson <mjohnson24@comcast.net>
2024-08-31 22:12:30 -0500

    cleaned up .filter method to pass filterFunction const to it instead

--- src/index.js
+++ src/index.js
@@ -9,9 +9,12 @@ const before = document.getElementById("before");
 // const manipulatedData = companiesData.push(extraRecord); // adds an element to the end
 // const manipulatedData = companiesData.unshift(extraRecord); // adds an element to the beginning
 
-const manipulatedData = companiesData.filter(function(company){
-	return company.fieldData.State === "TX" || company.fieldData.State === "FL"; 
-}); // filters the data based on the condition for data with a state of texas or florida
+const filterFunction = (company) => {
+	console.log(company);
+	return company.fieldData.State === "TX";
+}
+
+const manipulatedData = companiesData.filter(filterFunction); // filters the data based on the condition for data with a state of texas or florida
 
 //TODO: add the manipulated data array to the 'after' div.
 before.innerHTML = JSON.stringify(companiesData, null, 2);
