26ef3e18e29622d6e661b28a87f14eca8d2134f2
mark johnson <mjohnson24@comcast.net>
2024-08-31 19:53:57 -0500

    added filter method to filter data based on condition

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ const before = document.getElementById("before");
 // const manipulatedData = companiesData.pop(); // removes the last element
 // const manipulatedData = companiesData.shift(); // removes the first element
 // const manipulatedData = companiesData.push(extraRecord); // adds an element to the end
-const manipulatedData = companiesData.unshift(extraRecord); // adds an element to the beginning
+// const manipulatedData = companiesData.unshift(extraRecord); // adds an element to the beginning
+
+const manipulatedData = companiesData.filter(function(company){
+	return company.fieldData.State === "TX" || company.fieldData.State === "FL"; 
+}); // filters the data based on the condition for data with a state of texas or florida
+
 //TODO: add the manipulated data array to the 'after' div.
 before.innerHTML = JSON.stringify(companiesData, null, 2);
 const after = document.getElementById("after");
