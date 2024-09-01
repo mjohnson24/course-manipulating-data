0297642b7de2c022d546a947c47dabae792eb23b
mark johnson <mjohnson24@comcast.net>
2024-08-31 19:29:10 -0500

    added remove element lines

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { companiesData } from "./companies-data";
 
 const before = document.getElementById("before");
-before.innerHTML = JSON.stringify(companiesData, null, 2);
 
-const manipulatedData = [];
+// const manipulatedData = companiesData[0];
+// const manipulatedData = companiesData.pop(); // removes the last element
+const manipulatedData = companiesData.shift(); // removes the first element
 //TODO: add the manipulated data array to the 'after' div.
+before.innerHTML = JSON.stringify(companiesData, null, 2);
 const after = document.getElementById("after");
 after.innerHTML = JSON.stringify(manipulatedData, null, 2);
