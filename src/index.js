5beb929082547f3e9a22afcc99c3855392ebab1b
mark johnson <mjohnson24@comcast.net>
2024-09-07 19:43:53 -0500

    example of chaining methods with filter and map

--- src/index.js
+++ src/index.js
@@ -20,7 +20,10 @@ const updateEachElement = (company) => {
 	company.fieldData.CityState = `${company.fieldData.City}, ${company.fieldData.State}`;
 };
 // companiesData.forEach(updateEachElement);
-const manipulatedData = [...new Set ( companiesData.map ( (e) => { return e.fieldData.State}))].sort(); // removes duplicates. The (...) will append the data to the array. (Set) will remove duplicates.
+// const manipulatedData = [...new Set ( companiesData.map ( (e) => { return e.fieldData.State}))].sort(); // removes duplicates. The (...) will append the data to the array. (Set) will remove duplicates.
+
+// Filter function must return an array because the map function needs an array to work.
+const manipulatedData = companiesData.filter(filterFunction).map(mappedFunction); // filters the data and apply map with our mappedFunction.
 
 //TODO: add the manipulated data array to the 'after' div.
 before.innerHTML = JSON.stringify(companiesData, null, 2);
